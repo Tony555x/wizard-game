e91098c346bc64727b6bd9d61ea6c8e554f62c97
Tony555x <antonkremov2007@gmail.com>
2025-12-05 11:03:01 +0200

    Add icicle spell

---
+++ Spells/Icicle.cs
@@ -0,0 +1,12 @@
+namespace WizardGame.Spells{
+    public class Icicle:Spell{
+        public string Name => "Icicle";
+        public string Description => $"An icicle that deals ATK damage and lowers enemy attack. Costs 3 MP";
+        public int Cost => 3;
+        public override void Use(Wizard wizard, Enemy enemy){
+            enemy.TakeDamage(wizard.ATK);  
+            enemy.ATK -= 2;
+            Console.WriteLine($"{enemy.Name} lost 2 ATK.");
+        }
+    }
+}
