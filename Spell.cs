97059884cb2a347a7b90bca6065b11e3c6822191
Tony555x <antonkremov2007@gmail.com>
2025-12-05 11:00:25 +0200

    Add base spell class

---
+++ Spell.cs
@@ -0,0 +1,8 @@
+namespace WizardGame{
+    public abstract class Spell{
+        public string Name => "None";
+        public string Description => "None";
+        public int Cost => 0;
+        //public abstract void Use(Wizard wizard, Enemy enemy);
+    }
+}
