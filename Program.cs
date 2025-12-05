4aab22fd8b1f2f358578f13823f89cd6171b5ce6
Tony555x <antonkremov2007@gmail.com>
2025-12-05 10:59:33 +0200

    Add game start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace WizardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print("Wizard Game");
+            print("Press any key to start");
+            Console.ReadKey();
+            print("Choose a spell to learn.");
+            print("1. Fireball");
+            print("2. Icicle");
+            print("3. Lightning");
+            print("Enter number between 1 and 3.");
+            int spell=0;
+            while (true)
+            {
+                try{
+                    spell=int.Parse(Console.ReadLine());
+                    if (spell>=1 && spell<=3){
+                        break;
+                    }
+                }
+                catch{
+                    print("Enter a number between 1 and 3.");
+                }
+            }
+            Console.Clear();
+            print("You have entered the forest.");
+            print("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            print("You see a goblin.");
+        }
+        static void print(string a){
+            Console.WriteLine(a);
+        }
+    }
+}
